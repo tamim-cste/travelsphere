4aebf634df0eb418893c9490895b57f73e85a712
farman.arefin <farman.arefin1@outlook.com>
2026-06-08 10:55:31 +0600

    Initialize router for controllers

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,20 @@
 package routers
 
 import (
-	"travelsphere/controllers"
-	beego "github.com/beego/beego/v2/server/web"
+    "travelsphere/controllers"
+
+    beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+
+    beego.Router("/", &controllers.HomeController{})
+
+    beego.Router("/countries", &controllers.CountryController{}, "get:GetCountries")
+
+    beego.Router("/countries/:slug", &controllers.CountryController{}, "get:GetCountryDetail")
+
+    beego.Router("/wishlist", &controllers.WishlistController{})
+
+    beego.Router("/dashboard", &controllers.DashboardController{})
 }
